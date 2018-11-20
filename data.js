7f9733f6ca54cdd3f3ae132eeae2e9301b6b6e36
pierre.bouteloup <p.bouteloup@b-p.fr>
2018-11-20 13:06:35 +0100

    cleaning

---
+++ data.js
@@ -0,0 +1,39 @@
+  // Data
+  var data = [];
+
+  for (var i = 0; i < 75; ++i) {
+    data.push({
+      typo: ['bur', 'pc', 'lt'][Math.floor(3 * Math.random())],
+      level: ['RdC', 'SS1', 'R+1', 'R+2', 'R+3'][Math.floor(5 * Math.random())],
+      surface: Math.round(10 * Math.random(), 2)
+    });
+  }
+
+  data.sort((r1, r2) => r1.typo.localeCompare(r2.typo));
+  data.sort((r1, r2) => r1.level.localeCompare(r2.level));
+
+  data = data.reduce((levels, room) => {
+    var i = -1;
+    if (!~(i = levels.findIndex(level => level.name === room.level))) {
+      levels.push({
+        name: room.level,
+        rooms: [{
+          surface: room.surface,
+          typo: room.typo
+        }],
+        surface: 0
+      });
+      levels[levels.length - 1].surface += room.surface;
+    } else {
+      levels[i].rooms.push({
+        surface: room.surface,
+        typo: room.typo
+      });
+      levels[i].surface += room.surface;
+    }
+    return levels;
+  }, []);
+
+  var surfaceMax = d3.max(data.map(d => d.surface));
+
+  var levelCount = data.length;
